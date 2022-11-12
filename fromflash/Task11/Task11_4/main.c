df495e3b4edbd107c8344945b05a17304b0a73d8
PetrovMaksim2004 <maksman1987@mail.ru>
2022-11-12 18:52:35 +0500

    atm

--- fromflash/Task11/Task11_4/main.c
+++ fromflash/Task11/Task11_4/main.c
@@ -27,7 +27,7 @@ int mainMenu(ATM* atm, FILE* file)
 	int usersCount = countLinesInFile(file);
 	if (isUserInDataBase(atm, cardNumber, usersCount))
 	{
-		printf_s("No such user in database");
+		printf_s("No such user in database\n");
 		system("pause");
 		return 1;
 	}
@@ -41,7 +41,7 @@ int mainMenu(ATM* atm, FILE* file)
 	}
 	system("cls");
 	printf_s("Password is correct, choose the option\n");
-	printf_s("1.Get cash\n2.Insert cash\n3. Pay for mobile communication\n4. Exit\n");
+	printf_s("1. Get cash\n2. Insert cash\n3. Pay for mobile communication\n4. Exit\n");
 	char action = _getch();
 	switch (action)
 	{
@@ -57,7 +57,7 @@ int mainMenu(ATM* atm, FILE* file)
 		case '4':
 			return 0;
 		default:
-			printf_s("Wrong command!");
+			printf_s("Wrong command!\n");
 			return 1;
 	}
 	free(cardNumber);
