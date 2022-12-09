940341050361143e90c94cfeec18d0dffe70e471
PetrovMaksim2004 <maksman1987@mail.ru>
2022-12-10 00:34:25 +0500

    1

---
+++ fromflash/Task1/Task1_2/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main(void) {
+    int a = 26;
+    int b = 11;
+
+    printf_s("%d + %d = %d\n", a, b, a + b);
+    printf_s("%d - %d = %d\n", a, b, a - b);
+    printf_s("%d * %d = %d\n", a, b, a * b);
+    printf_s("%d / %d = %d\n", a, b, a / b);
+    printf_s("%d %% %d = %d\n", a, b, a % b);
+
+    return 0;
+}
---
+++ fromflash/Task1/Task1_3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+
+int main(void) {
+    setlocale(LC_ALL, "ru-RU");
+
+    int number = 0;
+    printf_s("Enter an integer: ");
+    if (!scanf_s("%d", &number)) {
+        printf_s("ERROR: wrong input, exit with code 1");
+        return 1;
+    }
+
+    if (number % 2) {
+        printf_s("Нечётное");
+    }
+    else {
+        printf_s("Чётное");
+    }
+    printf_s("\n");
+
+    system("pause");
+    return 0;
+}
